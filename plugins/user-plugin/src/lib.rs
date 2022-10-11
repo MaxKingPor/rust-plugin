c9414a3ee0f939a21b0cce262b207a24b8cb2806
MaxKingPor <1512347620@qq.com>
2022-10-11 12:47:03 +0800

    win DllMain

--- plugins/user-plugin/src/lib.rs
+++ plugins/user-plugin/src/lib.rs
@@ -23,3 +23,38 @@ impl UserPluginTrait for UserPlugin {
 pub fn get_user_plugin_trait() -> Box<dyn UserPluginTrait> {
     Box::new(UserPlugin {})
 }
+
+#[cfg(windows)]
+use std::ffi::c_void;
+#[cfg(windows)]
+use windows::Win32::Foundation::HINSTANCE;
+
+#[cfg(windows)]
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "system" fn DllMain(
+    hinstDLL: HINSTANCE,
+    fdwReason: u32,
+    lpvReserved: *const c_void,
+) -> bool {
+    use windows::Win32::System::SystemServices::{
+        DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH, DLL_THREAD_ATTACH, DLL_THREAD_DETACH,
+    };
+
+    match fdwReason {
+        DLL_PROCESS_ATTACH => {
+            println!("Dll {:?} DLL_PROCESS_ATTACH {:p}", hinstDLL, lpvReserved);
+        }
+        DLL_PROCESS_DETACH => {
+            println!("Dll {:?} DLL_PROCESS_DETACH {:p}", hinstDLL, lpvReserved);
+        }
+        DLL_THREAD_ATTACH => {
+            println!("Dll {:?} DLL_THREAD_ATTACH {:p}", hinstDLL, lpvReserved);
+        }
+        DLL_THREAD_DETACH => {
+            println!("Dll {:?} DLL_THREAD_DETACH {:p}", hinstDLL, lpvReserved);
+        }
+        _ => panic!(),
+    };
+    true
+}
